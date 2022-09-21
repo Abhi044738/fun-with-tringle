970c8c762a0968cbd2d085b99205ec4cd1c96210
Abhi044738 <110322117+Abhi044738@users.noreply.github.com>
2022-09-21 06:44:48 +0530

    fix: button

--- istriangle.js
+++ istriangle.js
@@ -1,21 +1,28 @@
-const inputs= document.querySelectorAll(".angle-input")
-const isTringleBtn=document.querySelector("#is-tringle-btn")
-const outputE1 =document.querySelector("#output")
+const inputs = document.querySelectorAll(".angle-input");
+//console.log(inputs);
+const isTringleBtn = document.querySelector("#is-triangle-btn");
+const outputE1 = document.querySelector("#output");
 
-function calculateSumOfAngles(angle1,angle2,angle3){
-    const sumOfAngles= angle1+angle2+angle3
-    //console.log(sumOfAngles)
-    return sumOfAngles
+function calculateSumOfAngles(angle1, angle2, angle3) {
+  const sumOfAngles = angle1 + angle2 + angle3;
+  //console.log(sumOfAngles);
+  return sumOfAngles;
 }
 
-function isTringle (){
-    const sumOfAngles=calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value))
-    if (sumOfAngles===180){
-        outputE1.innerText="The angles form a Tringle."
-    }else{
-        outputE1.innerText="The angles does not form Tringle."
-    }
-    
+function isTringle() {
+  //console.log("gggg");
+  const sumOfAngles = calculateSumOfAngles(
+    Number(inputs[0].value),
+    Number(inputs[1].value),
+    Number(inputs[2].value)
+  );
+  if (sumOfAngles === 180) {
+    //console.log("the angle form a triangle");
+    outputE1.innerText = "The angles form a Tringle.";
+  } else {
+    //console.log("the angle noy form a triangle");
+    outputE1.innerText = "The angles does not form Tringle.";
+  }
 }
 
-isTringleBtn.addEventListener("click",isTringle)
+isTringleBtn.addEventListener("click", isTringle);
