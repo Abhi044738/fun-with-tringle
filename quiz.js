a8b11b0544b9b93b486892623d580101abef63bc
Abhi044738 <110322117+Abhi044738@users.noreply.github.com>
2022-10-04 11:38:43 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,20 +1,21 @@
 const quizForm = document.querySelector(".quiz-form");
-const submitAnswerBtn= document.querySelector("#submit-answer-btn");
-const outputE1= document.querySelector("#output")
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const outputE1 = document.querySelector("#output");
 
-const correctAnswer=["90","right angled"]
+const correctAnswer = ["90", "right angled"];
 
-function calculateScore(){
-    let score=0;
-    let index=0;
-    const formResults =new FormData(quizForm);
-    for(let value of formResults.values()){
-        if (value === correctAnswer[index]){
-            score =score+1;
-        }
-        index=index+1;
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formResults = new FormData(quizForm);
+  console.log(formResults.values);
+  for (let value of formResults.values()) {
+    if (value === correctAnswer[index]) {
+      score = score + 1;
     }
-    outputE1.innerText ="Your score is "+score
+    index = index + 1;
+  }
+  outputE1.innerText = "Your score is " + score;
 }
 
-submitAnswerBtn.addEventListener("click",calculateScore);
+submitAnswerBtn.addEventListener("click", calculateScore);
